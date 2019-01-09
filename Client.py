c36f1f86a52761ab3504c214782ab3b31f7a9397
Alex Shoyhit <shoalex@gmail.com>
2019-01-09 19:33:22 +0200

    code improved

--- Client.py
+++ Client.py
@@ -1,24 +1,23 @@
 from tkinter import *
 import socket
-import threading
 import time
 
 def Server():
+
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
-
             for i in FileContent:
                 time.sleep(1)
-                text.insert(END, 'send to server {} {}'.format(i, "\n"))
+                text.insert(END, 'send to server {}\n'.format(i))
                 s.sendall(i.encode())
                 data = s.recv(1024)
-                text.insert(END, 'Received ftom server {} {}'.format(data.decode(), "\n"))
-                # print('Received', data.decode())
+                text.insert(END, 'Received ftom server {}\n'.format(data.decode()))
+                text.see(END)
     except:
         text.insert(END, "please connect to the server\n")
+        text.see(END)
         print("please connect to the server")
-        # exit(1)
 
 
 
@@ -27,24 +26,28 @@ PORT = 65432        # The port used by the server
 
 mainWindow=Tk()
 mainWindow.title("Water Station")
+scrollbar = Scrollbar(mainWindow)
+scrollbar.pack(side=RIGHT, fill=Y)
 text=Text(mainWindow)
+text.config(yscrollcommand=scrollbar.set)
 text.pack()
+scrollbar.config(command=text.yview)
 StartButton=Button(mainWindow,text="start",command=Server)
 StartButton.pack()
 
-
+file=""
 try:
     text.insert(END,"try to open file status.txt\n")
     file=open("status.txt","r")
     text.insert(END, "files opened\n")
     FileContent=file.read()
-
     text.insert(END,FileContent)
     FileContent=FileContent.split("\n")
-    file.close()
     text.insert(END, "\ncontent file saved and closed\n")
 except:
     text.insert(END, "file status.txt not exist\n")
-    #exit(1)
-
+finally:
+    if not file.closed:
+        file.close()
+text.insert(END, "Press Start and Please wait for server answer\n")
 mainWindow.mainloop()
